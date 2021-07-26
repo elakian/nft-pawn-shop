65ccd710d9c33bbc567bb82e9f4f78f0602b1d56
Kevin Huo <kevin.w.huo@gmail.com>
2021-07-26 14:47:03 -0400

    basic undo and payback

--- contracts/NFTPawnShop.sol
+++ contracts/NFTPawnShop.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract NFTPawnShop is IERC721Receiver, ERC721Holder {
+    using SafeMath for uint256;
     enum CollateralStatus {
         WAITING,
         ACTIVE,
@@ -56,7 +58,7 @@ contract NFTPawnShop is IERC721Receiver, ERC721Holder {
     function acceptTerms(address borrower, uint256 termIndex) public payable {
         Terms storage terms = borrowersToTerms[borrower][termIndex];
         uint256 amount = terms.amountInWei;
-        require(amount == msg.value, 'need to send the required wei');
+        require(amount == msg.value, "need to send the required wei");
         payable(terms.borrower).transfer(msg.value);
         terms.lender = msg.sender;
         terms.status = CollateralStatus.ACTIVE;
@@ -66,13 +68,69 @@ contract NFTPawnShop is IERC721Receiver, ERC721Holder {
 
     function claimCollateral(uint256 termIndex) external {
         Terms storage terms = lendersToTerms[msg.sender][termIndex];
-        require(terms.lender == msg.sender, 'you must be the lender');
+        require(terms.lender == msg.sender, "you must be the lender");
         require(
-            block.timestamp > terms.startTime + terms.duration, 
-            'pay back date should be in past'
+            block.timestamp > terms.startTime + terms.duration,
+            "pay back date should be in past"
         );
-        require (terms.status == CollateralStatus.ACTIVE, 'should be active');
-        terms.nftAddress.safeTransferFrom(address(this), msg.sender, terms.nftTokenID);
+        require(terms.status == CollateralStatus.ACTIVE, "should be active");
+        terms.nftAddress.safeTransferFrom(
+            address(this),
+            msg.sender,
+            terms.nftTokenID
+        );
         terms.status = CollateralStatus.DEFAULTED;
+    }
+
+    /// @dev undo term that has not been accepted yet
+    function undoTerm(address _borrower, uint256 termIndex) public {
+        Terms storage terms = borrowersToTerms[_borrower][termIndex];
+        address lender = terms.lender;
+        require(msg.sender == _borrower, "only borrower can call");
+        require(lender == address(0x0), "lender already set");
+        IERC721 nftAddress = terms.nftAddress;
+        uint256 nftTokenID = terms.nftTokenID;
+        nftAddress.safeTransferFrom(address(this), _borrower, nftTokenID);
+        delete borrowersToTerms[_borrower];
+    }
+
+    /// @dev calculate amount of payment required - interest rate in bps
+    function calculatePayment(
+        uint256 _amountInWei,
+        uint256 _duration,
+        uint256 _interestRate,
+        uint256 _startTime
+    ) public view returns (uint256 amount) {
+        uint256 currentDuration = block.timestamp.sub(_startTime).div(
+            1 minutes
+        );
+        uint256 numPayments = currentDuration.div(1); // per min
+        uint256 interest = _amountInWei.mul(_interestRate).div(10**4).mul(
+            numPayments
+        );
+        return _amountInWei.add(interest);
+    }
+
+    /// @dev refund
+    function refund(uint256 delta) public {
+        //TODO
+    }
+
+    /// @dev payback term
+    function paybackTerm(address _borrower, uint256 termIndex) public payable {
+        Terms memory terms = borrowersToTerms[_borrower][termIndex];
+        uint256 payment = calculatePayment(
+            terms.amountInWei,
+            terms.duration,
+            terms.interestRate,
+            terms.startTime
+        );
+        require(payment <= msg.value, "need to send min payback amount");
+        uint256 delta = msg.value - payment;
+        if (delta > 0) {
+            refund(delta);
+        }
+        payable(terms.lender).transfer(msg.value);
+        terms.status = CollateralStatus.RETURNED;
     }
 }
--- test/NFTPawnShop.ts
+++ test/NFTPawnShop.ts
@@ -125,18 +125,44 @@ describe("Pawning", () => {
     expect(await pawnNFT.ownerOf(1)).to.equal(await accounts[1].getAddress());
   });
 
-  it("claim collateral before due date", async () => {
+  it("claim collateral after due date", async () => {
     await basicPawn();
     const borrower = await accounts[0].getAddress();
     const acceptTermsTx = await pawnShop
       .connect(accounts[1]) // loaner
       .acceptTerms(borrower, 0, { value: 100000000 });
     await acceptTermsTx.wait();
+    await network.provider.send("evm_increaseTime", [4000]);
+    expect(await pawnNFT.ownerOf(1)).to.not.equal(
+      await accounts[1].getAddress()
+    );
+    const claimTx = await pawnShop
+      .connect(accounts[1]) // loaner
+      .claimCollateral(0);
+    await claimTx.wait();
+    expect(await pawnNFT.ownerOf(1)).to.equal(await accounts[1].getAddress());
+  });
+
+  it("basic undo term", async () => {
+    await basicPawn();
+    const borrower = await accounts[0].getAddress();
+    const prevBalance = await accounts[0].getBalance();
+    const undoTermsTx = await pawnShop
+      .connect(accounts[0]) // borrower
+      .undoTerm(borrower, 0);
+    await undoTermsTx.wait();
+    expect(await pawnNFT.ownerOf(1)).to.equal(borrower);
+  });
+  
+
+  it("basic undo must be from borrower", async () => {
+    await basicPawn();
+    const borrower = await accounts[0].getAddress();
     try {
-      const claimTx = await pawnShop
+      const acceptTermsTx = await pawnShop
         .connect(accounts[1]) // loaner
-        .claimCollateral(0);
-      await claimTx.wait();
+        .undoTerm(borrower, 0);
+      await acceptTermsTx.wait();
       expect(false);
     } catch (e) {
       expect(true);
@@ -144,5 +170,57 @@ describe("Pawning", () => {
     expect(await pawnNFT.ownerOf(1)).to.not.equal(
       await accounts[1].getAddress()
     );
+  });
+
+  it("basic payment calculations with interest rate ", async () => {
+    await basicPawn();
+    const currTimeStamp = Date.now() / 1000 | 0;
+    const earlierOneMinTimeStamp = currTimeStamp - 60; // 60 seconds prior (1 cycle) -> interest = 100000*(10/10000)*1 = 100
+    const earlierTwoMinTimeStamp = currTimeStamp - 120; // 2 mins prior (2 cycles) -> interest = 100000*(10/10000)*2 = 200
+
+    const paymentAmountOneMin = await pawnShop.calculatePayment(100000, 10, 10, earlierOneMinTimeStamp);
+    expect(paymentAmountOneMin.eq(100000+100)).to.equal(true);
+
+    const paymentAmountTwoMin = await pawnShop.calculatePayment(100000, 10, 10, earlierTwoMinTimeStamp);
+    expect(paymentAmountTwoMin.eq(100000+200)).to.equal(true);
+  });
+
+  it("should do basic payment calc", async () => {
+    await basicPawn();
+    const borrower = await accounts[0].getAddress();
+    const lender = await accounts[1].getAddress();
+    const initialBalanceBorrower = await accounts[0].getBalance();
+    const initialBalanceLender = await accounts[1].getBalance();
+    const initialBalanceBorrowerInt = await initialBalanceBorrower.toBigInt();
+    const initialBalanceLenderInt = await initialBalanceLender.toBigInt();
+    console.log("initialBalanceBorrowerInt: ", initialBalanceBorrowerInt);
+    console.log("initialBalanceLenderInt: ", initialBalanceLenderInt);
+    const acceptTermsTx = await pawnShop
+      .connect(accounts[1]) // loaner
+      .acceptTerms(borrower, 0, { value: 100000000 });
+    await acceptTermsTx.wait();
+    console.log("done with acceptTermsTx..");
+    const acceptBalanceBorrower = await accounts[0].getBalance();
+    const acceptBalanceLender = await accounts[1].getBalance();
+    const acceptBalanceBorrowerInt = await acceptBalanceBorrower.toBigInt();
+    const acceptBalanceLenderInt = await acceptBalanceLender.toBigInt();
+    console.log("acceptBalanceBorrowerInt: ", acceptBalanceBorrowerInt);
+    console.log("acceptBalanceLenderInt: ", acceptBalanceLenderInt);
+
+    const paybackTermsTx = await pawnShop.connect(accounts[0]).paybackTerm(borrower, 0, { value: 110000000});
+    await paybackTermsTx.wait();
+
+    const paybackBalanceBorrower = await accounts[0].getBalance();
+    const paybackBalanceLender = await accounts[1].getBalance();
+    const paybackBalanceBorrowerInt = await paybackBalanceBorrower.toBigInt();
+    const paybackBalanceLenderInt = await paybackBalanceLender.toBigInt();
+    
+    console.log("paybackBalanceBorrowerInt: ", paybackBalanceBorrowerInt);
+    console.log("paybackBalanceLenderInt: ", paybackBalanceLenderInt);
+    const finalDiffBorrower = await paybackBalanceBorrower.sub(acceptBalanceBorrower).toBigInt();
+    console.log("This is final diff for borrower side: ", finalDiffBorrower);
+    const finalDiffLender = await paybackBalanceLender.sub(acceptBalanceLender).toBigInt();
+    console.log("This is final diff for lender side: ", finalDiffLender);
+    expect(paybackBalanceLender.sub(acceptBalanceLender).eq(110000000)).to.equal(true);
   });
 });
